09f42b7cdeac5e922df22f672a9c0f1395b21b2c
Alterego11 <alterego15071970@gmail.com>
2022-06-01 23:16:05 +0300

    Create project

---
+++ TestScrollView/TestScrollView/TestScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestScrollView
+//
+//  Created by Антон Белый on 01.06.2022.
+//
+
+import UIKit
+import Stevia
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
