83232711f1988d756ca17e3db3243b2666ecc67b
Alterego11 <alterego15071970@gmail.com>
2022-06-02 23:29:36 +0300

    Finished

--- TestScrollView/TestScrollView/TestScrollView/ViewController.swift
+++ TestScrollView/TestScrollView/TestScrollView/ViewController.swift
@@ -4,17 +4,59 @@
 //
 //  Created by Антон Белый on 01.06.2022.
 //
-
-import UIKit
 import Stevia
+import UIKit
 
 class ViewController: UIViewController {
+    
 
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        
+        
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .yellow
+        let contentView = UIView()
+        contentView.backgroundColor = .blue
+        
+        let testView0 = UIView()
+        testView0.backgroundColor = .green
+        
+        let testView1 = UIView()
+        testView1.backgroundColor = .red
+            
+        view.subviews(
+            scrollView.subviews(
+                contentView.subviews(
+                    testView0,
+                    testView1
+                )
+            )
+        )
+        
+        scrollView.contentInsetAdjustmentBehavior = .never
+
+        let bounds = UIScreen.main.bounds
+        let height = bounds.size.height
+        
+        contentView.fillContainer()
+        contentView.Width == scrollView.Width
+        contentView.Height >= scrollView.Height
+        
+        scrollView.fillContainer()
+        
+        contentView.layout(
+            testView0.fillHorizontally().top(0).height(3500),
+            testView1.fillHorizontally(padding: 25).bottom(15).height(60).Top >= testView0.Bottom + 45
+        )
+ 
+        
+        self.view = view
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .darkGray
     }
-
-
 }
-
